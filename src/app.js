8b7b1372b770f52dca0fcead194b70fe2acfb329
jall0101 <92348560+jall0101@users.noreply.github.com>
2022-10-13 23:29:07 -0600

    MODIFIACIONES
    Se pusieron algunos ejemplos dentro de las carpetas para guiarnos

--- src/app.js
+++ src/app.js
@@ -3,18 +3,23 @@ const morgan = require('morgan');
 const db = require('./configuraciones/db');
 const Modelos = require('./modelos');
 const app = express();
+
+
 app.set('port', 3001);
+//app.use(morgan("dev"));
 app.use(morgan("combined"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/api/', require('./route'));
+app.use('/api/', require('./rutas'));
 //aqui irian las rutas
 
 
+
+//INICIANDO SERVER
 app.listen(app.get('port'), () => {
     console.log('Servidor iniciado en el puerto ' + app.get('port'));
     db.authenticate().then(() => {
-        console.log('Conexión establecidad');
+        console.log('Conexión establecida');
         Modelos.CrearModelos();
     })
         .catch((error) => {
--- src/modelos/index.js
+++ src/modelos/index.js
@@ -1,3 +1,18 @@
+// const Telefono = require('./telefono')
+
+
 exports.CrearModelos = () => {
     //aqui irian los modelos a crear
+
+    /* +++Ejemplos+++
+
+    Tipo.sync().then(() => {
+        console.log('MODELO CREADO CORRECTAMENTE')
+    })
+        .catch((error) => {
+            console.log('ERROR AL CREAR EL MODELO')
+            console.log(error)
+        })
+    */
+
 }
