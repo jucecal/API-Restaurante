1526fc7bdd85c643b940af95a1b3830452540b07
jall0101 <92348560+jall0101@users.noreply.github.com>
2022-10-23 14:20:40 -0600

    .

--- src/route/Clientes.js
+++ src/route/Clientes.js
@@ -2,8 +2,11 @@ const { Router } = require('express');
 const controladorClientes = require('../controller/Clientes');
 const {body, query} = require('express-validator');
 const ruta = Router();
+
 ruta.get('/', controladorClientes.Inicio);
+
 ruta.get('/listar', controladorClientes.Listar);
+
 ruta.post('/guardar', 
 body('nombres').isLength({min: 3, max: 50}).withMessage('Debe escribir el nombre del tipo con una longitud de 3 - 50 caracteres'),
 controladorClientes.Guardar);
