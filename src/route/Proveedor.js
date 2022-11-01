dc5e5bceb3d410e2cce00e9f45f5d82a0c0c1859
Julio Caballero <jucecal24@hotmail.com>
2022-11-01 01:22:25 -0600

    RUTAS PROVEEDOR

---
+++ src/route/Proveedor.js
@@ -0,0 +1,24 @@
+const { Router } = require('express');
+const controladorProveedor = require('../controller/Proveedor');
+const {body, query} = require('express-validator');
+const ruta = Router();
+
+ruta.get('/', controladorProveedor.Inicio);
+
+ruta.get('/listar', controladorProveedor.Listar);
+
+ruta.get('/buscarId', 
+query('id').isInt().withMessage('Solo se aceptan valores enteros para el id'),
+controladorProveedor.buscarId);
+
+ruta.post('/guardar', 
+controladorProveedor.Guardar);
+
+ruta.put('/editar', 
+query('id').isInt().withMessage('Solo se aceptan valores enteros para el id'),
+controladorProveedor.Editar);
+
+ruta.delete('/eliminar', 
+query('id').isInt().withMessage('Solo se aceptan valores enteros para el id'),
+controladorProveedor.Eliminar);
+module.exports=ruta;
