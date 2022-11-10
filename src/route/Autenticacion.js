57633a0e5ec09482cc5c9c55fcddd63790ae5e37
jall0101 <92348560+jall0101@users.noreply.github.com>
2022-11-10 00:32:53 -0600

    ..

--- src/route/Autenticacion.js
+++ src/route/Autenticacion.js
@@ -15,9 +15,11 @@ rutas.put('/recuperarcontrasena',
     query('usuario')
         .notEmpty().withMessage('Debe escribir el correo o nombre de usuario')
         .isLength({ min: 3 }).withMessage('Debe escribir un correo o nombre de usuario de 3 caracteres como mínimo'),
+
     body('pin')
-        .notEmpty().withMessage('Debe escribir el correo o nombre de usuario')
+        .notEmpty().withMessage('Debe escribir el pin')
         .isLength({ min: 4, max: 4 }).withMessage('Debe escribir un pin de 4 caracteres'),
+        
     body('contrasena')
         .notEmpty().withMessage('Debe escribir la contraseña de usuario')
         .isLength({ min: 6, max: 12 }).withMessage('Debe escribir una contraseña de 6 - 12 caracteres'),
